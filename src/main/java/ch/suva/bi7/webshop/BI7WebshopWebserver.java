5cc58bd9db26de7229436e85a26aac3637dd94a6
Andi Bill <andreas.bill@suva.ch>
2026-07-21 13:45:40 +0200

    Login Page

--- src/main/java/ch/suva/bi7/webshop/BI7WebshopWebserver.java
+++ src/main/java/ch/suva/bi7/webshop/BI7WebshopWebserver.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+// https://jetty.org/docs/jetty/12.1/operations-guide/session/index.html
+
 public class BI7WebshopWebserver {
 
     public static void main(String[] args) throws Exception {
